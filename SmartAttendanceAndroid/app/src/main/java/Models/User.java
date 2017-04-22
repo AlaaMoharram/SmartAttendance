37a2fc137f325c9fd1a169c4e12d4b1ff43814f5
Dalia Maarek <daliamaarek@gmail.com>
2017-04-22 13:41:54 +0200

    adding retrofit

--- SmartAttendanceAndroid/app/src/main/java/Models/User.java
+++ SmartAttendanceAndroid/app/src/main/java/Models/User.java
@@ -7,6 +7,15 @@ public class User {
     String name;
     String username;
     String password;
+    int user_id;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
 
     public Integer getRole() {
         return role;
--- SmartAttendanceAndroid/app/src/main/java/com/example/sarah/smartattendance/OurAPI.java
+++ SmartAttendanceAndroid/app/src/main/java/com/example/sarah/smartattendance/OurAPI.java
@@ -1,4 +1,50 @@
 package com.example.sarah.smartattendance;
 
+import Models.Beacon;
+import Models.Room;
+import Models.Tutorial;
+import Models.User;
+import retrofit2.Callback;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
 public interface OurAPI {
+
+    //users
+
+    @GET("/users/{user_id}/tutorials/")
+    void getActiveTutorial(@Path("user_id") String id, Callback<User> callbWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWack);
+
+    @GET("/users/{user_id}/tutorials")
+    void getAllTutorials(@Path("user_id") String id, Callback<User> callback);
+
+    //tutorials
+
+    @FormUrlEncoded
+    @PUT("/tutorials/{room_id}")
+    void updateTutorial(@Path("room_id") String id, @Field("tutorial[name]") String name,
+    @Field("tutorial[isActive]") boolean isActive,
+            Callback<Tutorial> callback);
+
+    @GET("/tutorials/{room_id}/room/{name}")
+    void findRoom(@Path("room_id") String id, @Path("name") String name, Callback<Tutorial> callback);
+
+    @GET("/tutorials/{room_id}/users")
+    void getAllStudents(@Path("room_id") String id, Callback<User> callback);
+
+    //room
+
+    @GET("/rooms/{name}/")
+    void getRoom(@Path("name") String name, Callback<Room> callback);
+
+    @GET("/rooms/{name}/beacons")
+    void getBeacons(@Path("name") String name, Callback<Beacon> callback);
+
+//    @FormUrlEncoded
+//    @PUT("/attendances/{}")
+//    void updateAttendance()
 }
