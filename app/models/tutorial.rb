4108961a90501fdfb47acd1a717e8e39a6323742
AlaaMoharram <alaa.moharram14@gmail.com>
2017-04-25 00:24:51 +0200

    minor change

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -5,14 +5,14 @@ class Tutorial < ApplicationRecord
   has_many :users, through: UserTutorial
 
   validates_presence_of :name, :room_id
-  before_save :falsify_all_others
+  # before_save :falsify_all_others
 
-  private
+ #  private
 
-  def falsify_all_others
-    if isActive
-      Tutorial.where.not(id: id).where(room_id: room_id).update_all(isActive: false)
-		end
-	end
+ #  def falsify_all_others
+ #    if isActive
+ #      Tutorial.where.not(id: id).where(room_id: room_id).update_all(isActive: false)
+	# 	end
+	# end
 
 end
