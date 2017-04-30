1c7001083093ab3aa136e854972ac6d024568006
AlaaMoharram <alaa.moharram14@gmail.com>
2017-04-30 15:05:11 +0200

    fixed room name validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,11 @@ class Room < ApplicationRecord
 	has_many :tutorial
 	has_many :beacon
 
-	validates_presence_of :name
+	# accepted format example: C7.202
+	VALID_NAME_REGEX = /\A[a-zA-Z][1-9]+\.[0-9]+\z/i
+
+	validates :name, presence: true, format: { with: VALID_NAME_REGEX }
+	validates_uniqueness_of :name, :case_sensitive => false
+
 end
 
